a4c713708c46894736c95ba97ed81af4ac101650
kaandag <kaani@live.nl>
2022-03-29 21:42:55 +0200

    added styling

--- theme.dart
+++ theme.dart
@@ -13,24 +13,53 @@ final colorScheme = ColorScheme.fromSeed(
 final tmmrThemeData = ThemeData(
   // Define the default brightness and colors.
   colorScheme: colorScheme,
-
+  scaffoldBackgroundColor: colorScheme.background,
   // Define the default font family.
-  fontFamily: 'Georgia',
+  fontFamily: 'Karla',
 
   // Define the default `TextTheme`. Use this to specify the default
   // text styling for headlines, titles, bodies of text, and more.
   textTheme: const TextTheme(
-    headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-    headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-    bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+    headline1: TextStyle(fontSize: 80.0, fontWeight: FontWeight.bold, color: Colors.white),
+    headline2: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold, color: Colors.white),
+    headline3: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold, color: Colors.white),
+    headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.normal, color: Colors.white),
+    bodyText2: TextStyle(fontSize: 13.0, color: Colors.white),
   ),
+
   elevatedButtonTheme: ElevatedButtonThemeData(style:
     ButtonStyle(
         shape: MaterialStateProperty.all<RoundedRectangleBorder>(
             RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(18.0)
+                borderRadius: BorderRadius.circular(25.0)
             )
-        )
+        ),
+      padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+        const EdgeInsets.fromLTRB(0, 18, 0, 18)
+      )
     )
   ),
+
+    textButtonTheme: TextButtonThemeData(
+      style: ButtonStyle(
+        foregroundColor:  MaterialStateProperty.all<Color>(
+          Colors.white
+        )
+      )
+    ),
+
+    inputDecorationTheme: const InputDecorationTheme(
+      filled: true,
+      fillColor: Color(0xff1A1E1E),
+      hintStyle: TextStyle(
+          color: Colors.white,
+      ),
+      border: OutlineInputBorder(
+        borderSide: BorderSide.none,
+        borderRadius: BorderRadius.all(Radius.circular(0)),
+      ),
+      contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+      floatingLabelBehavior: FloatingLabelBehavior.never
+  ),
+
 );
