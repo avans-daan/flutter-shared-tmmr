9a6154aaf3981f56e8dc632cf9143cec00677175
Leon in 't Veld <leon3110l@gmail.com>
2022-03-29 16:12:34 +0200

    added colors and button themes

--- theme.dart
+++ theme.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
+final colorScheme = ColorScheme.fromSeed(
+    seedColor: const Color(0xFF114AB8),
+    brightness: Brightness.dark,
+    background: const Color(0xff202024),
+    primary: const Color(0xFF114AB8),
+    onPrimary: Colors.white,
+    error: const Color(0xFFD01B1B),
+);
+
 // TODO TMMR-59 determine the correct colours when building the shared widgets and layouts
 final tmmrThemeData = ThemeData(
   // Define the default brightness and colors.
-  brightness: Brightness.dark,
-  primaryColor: Colors.red[800],
+  colorScheme: colorScheme,
 
   // Define the default font family.
   fontFamily: 'Georgia',
@@ -15,5 +23,14 @@ final tmmrThemeData = ThemeData(
     headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
     headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
     bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+  ),
+  elevatedButtonTheme: ElevatedButtonThemeData(style:
+    ButtonStyle(
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(18.0)
+            )
+        )
+    )
   ),
 );
