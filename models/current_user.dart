c7b1031586e6cbd17b846585767cf45499a25cc9
Aremyre <Larsrijnen@gmail.com>
2022-05-10 15:05:53 +0200

    Fix light and dark theme and notifications and change colors

---
+++ models/current_user.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../http_client.dart';
+import '../theme.dart';
+import 'api-resources/user.dart';
+
+enum Themes { dark, light }
+
+extension ThemeDataExtension on Themes {
+  ThemeData getThemeData() {
+    switch (this) {
+      case Themes.dark:
+        return darkThemeSchema;
+      case Themes.light:
+        return lightThemeSchema;
+      default:
+        return darkThemeSchema;
+    }
+  }
+}
+
+@immutable
+class CurrentUser {
+  const CurrentUser({
+    required this.user,
+  });
+
+  final AsyncValue<User> user;
+
+  Themes getCurrentTheme() {
+    return user.when(
+        loading: () => Themes.dark,
+        error: (err, stack) => Themes.dark,
+        data: (user) {
+          if (user.settings != null && user.settings['theme'] != null) {
+            switch (user.settings['theme']) {
+              case 'dark':
+                return Themes.dark;
+              case 'light':
+                return Themes.light;
+            }
+          }
+
+          return Themes.dark;
+        }
+    );
+  }
+
+  bool getNotification() {
+    return user.when(
+        loading: () => false,
+        error: (err, stack) => false,
+        data: (user) {
+          if (user.settings != null && user.settings['notification'] != null) {
+            return true;
+          }
+
+          return false;
+        }
+    );
+  }
+
+  CurrentUser copyWith({
+    AsyncValue<User>? user,
+  }) {
+    return CurrentUser(
+      user: user ?? this.user,
+    );
+  }
+}
+
+class CurrentUserStateNotifier extends StateNotifier<CurrentUser> {
+  CurrentUserStateNotifier(CurrentUser state) : super(state);
+
+  Future<void> updateToApi() async {
+    var user = state.user.asData?.value;
+    if (user == null) return;
+
+    try {
+        final response = await HttpClient()
+          .getAuthorizedClient()
+          .put('/api/user', data: user.toMap());
+
+        if (response.statusCode != 200)  {
+          throw Exception('Could not save user');
+        }
+    } catch (err) {
+      // TODO Error handling user story?
+    }
+  }
+
+  Future<void> setCurrentTheme(Themes theme) async {
+      var user = state.user.asData?.value;
+      if (user == null) return;
+      var settings = user.settings;
+
+      switch (theme) {
+        case Themes.dark:
+          settings['theme'] = 'dark';
+          break;
+        case Themes.light:
+          settings['theme'] = 'light';
+          break;
+      }
+
+      var newUser = user.copyWith(settings: settings);
+      state = state.copyWith(user: AsyncValue.data(newUser));
+      await updateToApi();
+  }
+
+  Future<void> setNotifications(bool notification) async {
+    var user = state.user.asData?.value;
+    if (user == null) return;
+    var settings = user.settings;
+
+    settings['notification'] = notification;
+
+    var newUser = user.copyWith(settings: settings);
+    state = state.copyWith(user: AsyncValue.data(newUser));
+    await updateToApi();
+  }
+
+  static final provider = StateNotifierProvider<CurrentUserStateNotifier, CurrentUser>((ref) {
+    var user = ref.watch(fetcher);
+    return CurrentUserStateNotifier(CurrentUser(user: user));
+  });
+
+
+  static final fetcher = FutureProvider<User>((ref) async {
+    final response = await HttpClient()
+        .getAuthorizedClient()
+        .get('/api/user');
+    return User.fromJson(response.data['data']);
+  });
+}
+
+
+class CurrentUserThemeNotifier {
+  static final provider = StateProvider<Themes>((ref) {
+    var state = ref.watch(CurrentUserStateNotifier.provider.select((value) => value.getCurrentTheme()));
+    return state;
+  });
+}
--- theme.dart
+++ theme.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-final colorScheme = ColorScheme.fromSeed(
+final darkColorScheme = ColorScheme.fromSeed(
   seedColor: const Color(0xFF114AB8),
   brightness: Brightness.dark,
   secondary: const Color(0xff2d2f33),
@@ -9,15 +9,25 @@ final colorScheme = ColorScheme.fromSeed(
   primary: const Color(0xFF114AB8),
   onPrimary: Colors.white,
   error: const Color(0xFFD01B1B),
-  surface: const Color(0xFF1C1B1F),
-  shadow: const Color.fromARGB(77, 0, 0, 0)
 );
 
-// TODO TMMR-59 determine the correct colours when building the shared widgets and layouts
-final tmmrThemeData = ThemeData(
+final lightColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color(0xFF114AB8),
+  brightness: Brightness.light,
+  secondary: const Color(0xffa1a1a1),
+  onSecondary: const Color(0xff5e5e5e),
+  background: const Color(0xFFFFFFFF),
+  primary: const Color(0xFF114AB8),
+  onPrimary: Colors.white,
+  error: const Color(0xFFD01B1B),
+    surface: const Color(0xFFCCC6E2),
+    shadow: const Color.fromARGB(77, 0, 0, 0)
+);
+
+final darkThemeSchema = ThemeData(
   // Define the default brightness and colors.
-  colorScheme: colorScheme,
-  scaffoldBackgroundColor: colorScheme.background,
+  colorScheme: darkColorScheme,
+  scaffoldBackgroundColor: darkColorScheme.background,
   // Define the default font family.
   fontFamily: 'Karla',
 
@@ -37,6 +47,57 @@ final tmmrThemeData = ThemeData(
     headline6: TextStyle(
         fontSize: 10.0, fontStyle: FontStyle.normal, color: Colors.white),
     bodyText2: TextStyle(fontSize: 13.0, color: Colors.white),
+  ),
+
+  elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25.0))),
+          padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+              const EdgeInsets.fromLTRB(0, 18, 0, 18)))),
+
+  textButtonTheme: TextButtonThemeData(
+      style: ButtonStyle(
+          foregroundColor: MaterialStateProperty.all<Color>(Colors.white))),
+
+  inputDecorationTheme: const InputDecorationTheme(
+      filled: true,
+      fillColor: Color(0xff1A1E1E),
+      hintStyle: TextStyle(
+        color: Colors.white,
+      ),
+      border: OutlineInputBorder(
+        borderSide: BorderSide.none,
+        borderRadius: BorderRadius.all(Radius.circular(0)),
+      ),
+      contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+      floatingLabelBehavior: FloatingLabelBehavior.never),
+);
+
+final lightThemeSchema = ThemeData(
+  // Define the default brightness and colors.
+  colorScheme: lightColorScheme,
+  scaffoldBackgroundColor: lightColorScheme.background,
+  // Define the default font family.
+  fontFamily: 'Karla',
+
+  // Define the default `TextTheme`. Use this to specify the default
+  // text styling for headlines, titles, bodies of text, and more.
+  textTheme: const TextTheme(
+    headline1: TextStyle(
+        fontSize: 80.0, fontWeight: FontWeight.bold, color: Colors.black),
+    headline2: TextStyle(
+        fontSize: 28.0, fontWeight: FontWeight.bold, color: Colors.black),
+    headline3: TextStyle(
+        fontSize: 24.0, fontWeight: FontWeight.normal, color: Colors.black),
+    headline4: TextStyle(
+        fontSize: 18.0, fontWeight: FontWeight.normal, color: Colors.black),
+    headline5: TextStyle(
+        fontSize: 14.0, fontStyle: FontStyle.normal, color: Colors.black),
+    headline6: TextStyle(
+        fontSize: 10.0, fontStyle: FontStyle.normal, color: Colors.black),
+    bodyText2: TextStyle(fontSize: 13.0, color: Colors.black),
   ),
 
   elevatedButtonTheme: ElevatedButtonThemeData(
