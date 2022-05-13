a87106d2469e1a59b40dc6d484164abf65ac4a29
Max van de Vossenberg <maxvossenberg@gmail.com>
2022-05-13 16:34:29 +0200

    Fixed enum parsing

--- models/api-resources/time_entry.dart
+++ models/api-resources/time_entry.dart
@@ -18,7 +18,7 @@ class TimeEntry {
   final DateTime? stop;
   final Target? target;
   final int duration;
-  final States? state;
+  final States state;
 
   static TimeEntry fromJson(dynamic content) {
     return TimeEntry(
@@ -28,6 +28,6 @@ class TimeEntry {
         stop: content['stop'] != null ? DateTime.parse(content['stop']) : null,
         target: content['target'] != null ? Target.fromJson(content['target']) : null,
         duration: content['duration'],
-        state: States.values.where((e) => e.toString() == content['state']).first);
+        state: States.values.byName(content['state']));
   }
 }
