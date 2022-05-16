e44776e96f12e9c09486079650ecf2f655b1dafa
Max van de Vossenberg <maxvossenberg@gmail.com>
2022-05-16 20:27:56 +0200

    Added enum for state, and Copy with method for TimeEntry

--- models/api-resources/time_entry.dart
+++ models/api-resources/time_entry.dart
@@ -5,12 +5,12 @@ enum States { incomplete, complete, approved, synced }
 class TimeEntry {
   TimeEntry(
       {required this.id,
-        required this.description,
-        required this.start,
-        required this.stop,
-        required this.target,
-        required this.duration,
-        required this.state});
+      required this.description,
+      required this.start,
+      required this.stop,
+      required this.target,
+      required this.duration,
+      required this.state});
 
   final String id;
   final String? description;
@@ -26,8 +26,28 @@ class TimeEntry {
         description: content['description'],
         start: DateTime.parse(content['start']),
         stop: content['stop'] != null ? DateTime.parse(content['stop']) : null,
-        target: content['target'] != null ? Target.fromJson(content['target']) : null,
+        target: content['target'] != null
+            ? Target.fromJson(content['target'])
+            : null,
         duration: content['duration'],
         state: States.values.byName(content['state']));
+  }
+
+  TimeEntry copyWith(
+      {String? id,
+      String? description,
+      DateTime? start,
+      DateTime? stop,
+      Target? target,
+      int? duration,
+      States? state}) {
+    return TimeEntry(
+        id: id ?? this.id,
+        description: description ?? this.description,
+        start: start ?? this.start,
+        stop: stop ?? this.stop,
+        target: target ?? this.target,
+        duration: duration ?? this.duration,
+        state: state ?? this.state);
   }
 }
